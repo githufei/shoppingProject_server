c825495b9ea1e41d911fc7ef281796f93dab2343
githufei <460284524@qq.com>
2017-08-12 13:20:32 +0800

    'reData'

--- database/cleanDatabase.js
+++ database/cleanDatabase.js
@@ -1,4 +1,4 @@
-let {Order,User}=require('./model');
+let {Order,User,Product}=require('./model');
 // 清空订单列表
 Order.remove({},function(err,result){
     if(err){
@@ -10,13 +10,23 @@ Order.remove({},function(err,result){
     }
 })
 
-// 清空用户列表
-// User.remove({},function(err,result){
-//     if(err){
-//         console.log(err);
-//     }else{
-//         console.log(result.result);
-//         // {ok:1,n:2}
-//         // ok=1表示的是删除操作成功,n=0表示实际删除掉的记录条数为2
-//     }
-// })
+
+User.remove({},function(err,result){
+    if(err){
+        console.log(err);
+    }else{
+        console.log(result.result);
+        // {ok:1,n:2}
+        // ok=1表示的是删除操作成功,n=0表示实际删除掉的记录条数为2
+    }
+})
+
+Product.remove({},function(err,result){
+    if(err){
+        console.log(err);
+    }else{
+        console.log(result.result);
+        // {ok:1,n:2}
+        // ok=1表示的是删除操作成功,n=0表示实际删除掉的记录条数为2
+    }
+})
